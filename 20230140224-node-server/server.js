a9600dcc3ca5288b1cbd7f422926989c8e4f43ba
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-13 10:13:13 +0700

    first commit

--- 20230140224-node-server/server.js
+++ 20230140224-node-server/server.js
@@ -1,11 +1,11 @@
- const express = require('express');
+const express = require('express');
 const app = express();
-const port = 5000;
+const PORT = 5000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node.js Server!' });
+  res.json({ message: 'Hello from Server!' });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
